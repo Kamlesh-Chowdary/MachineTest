7c6f0f968335014dc97db955b68b992fbe637241
kamlesh-chowdary <kamleshchoyal143@gmail.com>
2024-07-26 18:06:46 +0530

    Add Register model

---
+++ backend/src/models/register.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const registerSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+registerSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+export const Register = mongoose.model("register", registerSchema);
