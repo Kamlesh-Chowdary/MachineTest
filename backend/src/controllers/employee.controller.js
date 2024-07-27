a6aa384014bc08e86b2f3d35fa53bf352cb89d71
kamlesh-chowdary <kamleshchoyal143@gmail.com>
2024-07-27 17:39:14 +0530

    add modify employee end pont

--- backend/src/controllers/employee.controller.js
+++ backend/src/controllers/employee.controller.js
@@ -1,104 +1,127 @@
-import {Employee} from "../models/employee.model.js";
+import { Employee } from "../models/employee.model.js";
 import ApiError from "../utils/apiError.js";
 import ApiResponse from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-
-const deleteImage = (filePath)=>{
-    FileSystem.unlink(filePath,(err)=>{
-        if(err){
-            throw new ApiError(409,"Error while deleting the image")
-        }else{
-            return true
-        }
-    })
-}
+import fs from "fs";
+const deleteImage = (filePath) => {
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      throw new ApiError(409, "Error while deleting the image");
+    } else {
+      return true;
+    }
+  });
+};
 
 const createEmployee = asyncHandler(async (req, res) => {
-    const {name, email, phoneNumber, designation, gender, course} = req.body;
-    if([name, email, phoneNumber, designation, gender,course].some(field=> field?.trim() === "")){
-        throw new ApiError(400, "All fields are required.")
-    }
-    const existingEmployee = await Employee.findOne({email});
-    if(existingEmployee){
-        throw new ApiError(409,"Employee with this email address already exists.")
-    }
-    const imageFilePath = req.file?.path;
-    if(!imageFilePath){
-        throw new ApiError(400, "Employee Image is required");
-    }
+  const { name, email, phoneNumber, designation, gender, course } = req.body;
+  if (
+    [name, email, phoneNumber, designation, gender, course].some(
+      (field) => field?.trim() === ""
+    )
+  ) {
+    throw new ApiError(400, "All fields are required.");
+  }
+  const existingEmployee = await Employee.findOne({ email });
+  if (existingEmployee) {
+    throw new ApiError(409, "Employee with this email address already exists.");
+  }
+  const imageFilePath = req.file?.path;
+  if (!imageFilePath) {
+    throw new ApiError(400, "Employee Image is required");
+  }
 
-    const employeeCreate = await Employee.create({
-        image: imageFilePath,
-        name,
-        email,
-        phoneNumber,
-        designation,
-        gender,
-        course
-    })
-    const createdEmployee = await Employee.findById(employeeCreate._id).select("-__v");
-    if(!createEmployee){
-        throw new ApiError(500, "Something went wrong while creating the employee")
-    }
+  const employeeCreate = await Employee.create({
+    image: imageFilePath,
+    name,
+    email,
+    phoneNumber,
+    designation,
+    gender,
+    course,
+  });
+  const createdEmployee = await Employee.findById(employeeCreate._id).select(
+    "-__v"
+  );
+  if (!createEmployee) {
+    throw new ApiError(500, "Something went wrong while creating the employee");
+  }
 
-    res.status(200).json( new ApiResponse(201, createdEmployee,"Employee created successfully"))
-})
+  res
+    .status(200)
+    .json(
+      new ApiResponse(201, createdEmployee, "Employee created successfully")
+    );
+});
 
-const getEmployees = asyncHandler( async (req, res) => {
-    const employee = await Employee.find({}).select("-__v");
-    if(!employee){
-        throw new ApiError(404, "Error while fetching employee details");
-    }
-    res.status(200).json(new ApiResponse(201, employee, "Employee Data fetched successfully"))
-})
+const getEmployees = asyncHandler(async (req, res) => {
+  const employee = await Employee.find({}).select("-__v");
+  if (!employee) {
+    throw new ApiError(404, "Error while fetching employee details");
+  }
+  res
+    .status(200)
+    .json(new ApiResponse(201, employee, "Employee Data fetched successfully"));
+});
 
 const modifyEmployee = asyncHandler(async (req, res) => {
-    const {employeeId} = req.params;
+  const { employeeId } = req.params;
 
-    if(!employeeId){
-        throw new ApiError(404,"Employee Id is required")
-    }
+  if (!employeeId) {
+    throw new ApiError(404, "Employee Id is required");
+  }
 
-    const employeeExist = await Employee.findById(employeeId);
+  const employeeExist = await Employee.findById(employeeId);
 
-    if(!employeeExist){
-        throw new ApiError(404,"Invalid Employee Id")
-    }
-    
-    const {name, email, phoneNumber, designation, gender, course} = req.body;
-    if([name, email, phoneNumber, designation, gender,course].some(field=> field?.trim() === "")){
-        throw new ApiError(400, "All fields are required.")
-    }
+  if (!employeeExist) {
+    throw new ApiError(404, "Invalid Employee Id");
+  }
 
-    deleteImage(employeeExist.image);
-
-    const imageFilePath = req.file?.path;
-    if(!imageFilePath){
-        throw new ApiError(400, "Employee Image is required");
-    }
-    const updateEmployee = await Employee.findByIdAndUpdate(
-        employeeId,
-        {
-            name,
-            email,
-            phoneNumber,
-            designation,
-            gender,
-            course,
-            image: imageFilePath
-         },
-        {
-            new: true
-        }
+  const { name, email, phoneNumber, designation, gender, course } = req.body;
+  if (
+    [name, email, phoneNumber, designation, gender, course].some(
+      (field) => field?.trim() === ""
     )
+  ) {
+    throw new ApiError(400, "All fields are required.");
+  }
+  const imageFilePath = req.file?.path;
+  if (!imageFilePath) {
+    throw new ApiError(400, "Employee Image is required");
+  }
+  if (employeeExist.image !== imageFilePath) {
+    deleteImage(employeeExist.image);
+  }
 
-    if(!updateEmployee){
-        throw new ApiError(404,"Error while updating the employee details")
+  const updateEmployee = await Employee.findByIdAndUpdate(
+    employeeId,
+    {
+      name,
+      email,
+      phoneNumber,
+      designation,
+      gender,
+      course,
+      image: imageFilePath,
+    },
+    {
+      new: true,
     }
+  );
+  console.log(updateEmployee);
+  if (!updateEmployee) {
+    throw new ApiError(404, "Error while updating the employee details");
+  }
 
-    res.status(200).json(201, updateEmployee, "Employee details updated successfully")
-})
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        201,
+        updateEmployee,
+        "Employee details updated successfully"
+      )
+    );
+});
 
-
-
-export {createEmployee, getEmployees, modifyEmployee}
+export { createEmployee, getEmployees, modifyEmployee };
--- backend/src/routes/employee.routes.js
+++ backend/src/routes/employee.routes.js
@@ -1,9 +1,16 @@
 import Routes from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { createEmployee, getEmployees } from "../controllers/employee.controller.js";
+import {
+  createEmployee,
+  getEmployees,
+  modifyEmployee,
+} from "../controllers/employee.controller.js";
 
 const employeeRoute = Routes();
 
-employeeRoute.route("/create").post(upload.single("image"),createEmployee)
-employeeRoute.route("/get-employee").get(getEmployees)
+employeeRoute.route("/create").post(upload.single("image"), createEmployee);
+employeeRoute.route("/get-employee").get(getEmployees);
+employeeRoute
+  .route("/modify/:employeeId")
+  .post(upload.single("image"), modifyEmployee);
 export default employeeRoute;
