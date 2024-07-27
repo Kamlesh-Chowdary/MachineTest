249fec0183579768ba91afeaecfe56eb7894fe19
kamlesh-chowdary <kamleshchoyal143@gmail.com>
2024-07-27 19:04:24 +0530

    add store

---
+++ frontend/src/store/employeeSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  employeeList: [],
+};
+
+const employeeSlice = createSlice({
+  name: "employee",
+  initialState,
+  reducers: {
+    setEmployees: (state, action) => {
+      state.employeeList = action.payload;
+    },
+
+    addEmployee: (state, action) => {
+      state.employeeList.push(action.payload);
+    },
+    modifyEmployee: (state, action) => {
+      state.employeeList = state.employeeList.map((employee) =>
+        employee._id === action.payload._id ? action.payload : employee
+      );
+    },
+    deleteEmployee: (state, action) => {
+      state.employeeList = state.employeeList.filter(
+        (employee) => employee._id !== action.payload._id
+      );
+    },
+  },
+});
+
+export const { setEmployees, addEmployee, modifyEmployee, deleteEmployee } =
+  employeeSlice.actions;
+
+export default employeeSlice.reducer;
